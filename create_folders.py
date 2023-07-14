a2a1ea43721c3bf85222740fddfe127bf2bdb778
Jared Hunter <jared.e.hunter@gmail.com>
2023-07-13 21:23:30 -0400

    refactored using pathlib

--- create_folders.py
+++ create_folders.py
@@ -1,33 +1,35 @@
-import os
+from pathlib import Path
 
 # path root
-root = './' # curently set to folder where the script resides
+root = Path("./samples/")  # curently set to folder where the script resides
 
 
 # Create a list of main folders, (ie, for articulations, instruments, ...)
 folders = [
-  'sustain',
-  'staccato',
-  'marcato',
-  'pizzicato',
-  'crescendo',
-  'diminuendo',
+    "sustain",
+    "staccato",
+    "marcato",
+    "pizzicato",
+    "crescendo",
+    "diminuendo",
 ]
 
 # Create a list of subfolders (ie, for round robins, dynamic levels, ...)
 subfolders = [
-  "RR1",
-  "RR2",
-  "RR3",
-  "RR4",
+    "RR1",
+    "RR2",
+    "RR3",
+    "RR4",
 ]
 
-# Loop over main and subfolders to make directories
-for folder in folders:
-    # maked the main folder
-    os.mkdir(f"{root}/{folder}")
 
-    # make the subfolders within each main folder
-    for subfolder in subfolders:
-        path = f"{root}/{folder}/{subfolder}"
-        os.mkdir(path)
+try:
+    # Loop over main and subfolders to make directories
+    for folder in folders:
+        for subfolder in subfolders:
+            subfolder_path = Path(root, folder, subfolder)
+            Path.mkdir(subfolder_path, parents=True, exist_ok=False)
+
+except FileExistsError:
+    print("One or more of the directories already exists.")
+    exit(1)
